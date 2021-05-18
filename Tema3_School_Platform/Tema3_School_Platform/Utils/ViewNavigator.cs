9367511d85a6bbfb6fd3075a85af4937da05cc7f
Pamfile Alex <damrosrak@gmail.com>
2021-05-18 20:28:15 +0300

    Added LoginPage for Tema3

---
+++ Tema3_School_Platform/Tema3_School_Platform/Utils/ViewNavigator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tema3_School_Platform.Views;
+
+namespace Tema3_School_Platform.Utils
+{
+    static class ViewNavigator
+    {
+        private static readonly LoginPage loginPage;
+        private static MainWindow mainWindow;
+        public static MainWindow MainWindow
+        {
+            set
+            {
+                //if (mainWindow != null) { return; }
+                mainWindow = value;
+            }
+        }
+
+        static ViewNavigator()
+        {
+            loginPage = new LoginPage();
+        }
+
+        public static void ChangeToLoginPage()
+        {
+            if (mainWindow == null) { return; }
+            mainWindow.Content = loginPage;
+        }
+    }
+}
---
+++ Tema3_School_Platform/Tema3_School_Platform/ViewModels/LoginPageVM.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+using Tema3_School_Platform.Commands;
+using Tema3_School_Platform.Utils;
+
+namespace Tema3_School_Platform.ViewModels
+{
+    class LoginPageVM : BasePropertyChanged
+    {
+        private String errorMessage;
+        public String ErrorMessage
+        {
+            get { return errorMessage; }
+            set
+            {
+                errorMessage = value;
+                NotifyPropertyChanged("Error");
+            }
+        }
+
+        private String username;
+        public String Username
+        {
+            get { return username; }
+            set
+            {
+                username = value;
+                NotifyPropertyChanged("Username");
+            }
+        }
+
+        public ICommand LoginCommand { get; }
+
+        public LoginPageVM()
+        {
+            LoginCommand = new ActionCommand(Login, CanLogin);
+        }
+
+        private void Login()
+        {
+
+        }
+
+        private bool CanLogin()
+        {
+            return !String.IsNullOrEmpty(Username);
+        }
+    }
+}
