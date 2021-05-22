df86d34264114cb9f89fa81854901b89f5fe0898
Pamfile Alex <damrosrak@gmail.com>
2021-05-22 09:20:29 +0300

    Partial design for ClassPage for Tema3

---
+++ Tema3_School_Platform/Tema3_School_Platform/ViewModels/ClassPageVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tema3_School_Platform.Models.EntityLayer;
+
+namespace Tema3_School_Platform.ViewModels
+{
+    class ClassPageVM
+    {
+        public ObservableCollection<Class> Classes { get; }
+    }
+}
